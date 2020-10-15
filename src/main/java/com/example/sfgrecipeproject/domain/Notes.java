2eb8aa3a6b336797d2ebbab82dac57931c1c8b2d
Connor <cfech44@gmail.com>
2020-10-14 21:37:17 -0400

    adding in notes and recipes pojos and their relationship fields

---
+++ src/main/java/com/example/sfgrecipeproject/domain/Notes.java
@@ -0,0 +1,43 @@
+package com.example.sfgrecipeproject.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    //this allows us to put in more than 255 characters, which is hibernates max
+    //stand for large object storage
+    @Lob
+    private String recipeNotes;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
